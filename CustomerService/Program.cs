e362ffae9d7b8b2c1d8e8cdc2d72d3e59a84f751
Rodrigo Sillos <rodrigosillos@gmail.com>
2024-11-04 00:14:08 -0300

    Cleanup

--- CustomerService/Program.cs
+++ CustomerService/Program.cs
@@ -3,21 +3,17 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar serviços para API e Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Registrar o MongoClient para injeção de dependência
 var connectionString = builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString");
 builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(connectionString));
 
-// Registrar o CustomerManagementService para injeção de dependência
 builder.Services.AddSingleton<CustomerManagementService>();
 
 var app = builder.Build();
 
-// Configuração do pipeline de requisições HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
